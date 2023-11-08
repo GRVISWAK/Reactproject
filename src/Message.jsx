02cb2fb9b2f131a7bfbbc0119c7d9c2104f3d972
GRVISWAK <727722euit214@skcet.ac.in>
2023-11-08 09:04:02 +0530

    commit

---
+++ src/Message.jsx
@@ -0,0 +1,10 @@
+function Message()
+{
+    return (
+        <div>
+            <h1>From Meassage.jsx</h1>
+            <p>hello world</p>
+        </div>
+    );    
+}
+export { Message };
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Navbar= () =>{
+    return(
+        <div>
+          <center>
+           <hr></hr>
+           <h1 style={{backgroundColor:"yellowgreen", fontSize:"30px" }}>VISWAK G R</h1>
+           <hr></hr>
+           </center>
+        </div>
+    );
+};
+
+
+export default Navbar;
